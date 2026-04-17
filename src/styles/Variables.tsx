c44c99dc7380d41a2bc659a00d890c69f068f39a
Felipe De Andrade <felipe.deam98@gmail.com>
2026-04-17 09:31:39 -0300

    chore(styles): add more options of variables colors

--- src/styles/Variables.tsx
+++ src/styles/Variables.tsx
@@ -3,6 +3,17 @@ import { createGlobalStyle } from "styled-components";
 const Variables = createGlobalStyle`
   :root {
 
+    --color-brand-primary: #5865f2;
+
+    --color-text-primary: #23272a;
+    
+    --color-button-inverse-hover: #36393f;
+    
+    --color-neutral-black: #000000;
+    --color-neutral-white: #ffffff;
+
+    --color-shadow: rgba(0, 0, 0, 0.2);
+    
     --footer: rgba(35, 39, 42, 1);
 
     --lighter: #ffffff;
