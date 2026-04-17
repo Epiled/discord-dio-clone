fc926e04c4c9c6738956007f416c2bac952c4831
Felipe De Andrade <felipe.deam98@gmail.com>
2026-04-17 09:33:10 -0300

    refactor(button): align button styles with specifications

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,13 +5,26 @@ type Appearance = "default" | "inverse";
 interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text?: string;
   appearance?: Appearance;
+  size?: "small" | "large";
   className?: string;
 }
 
 export const Button = (props: IButton) => {
-  const { text, appearance = "default", className, children, ...rest } = props;
+  const {
+    text,
+    appearance = "default",
+    size = "large",
+    className,
+    children,
+    ...rest
+  } = props;
   return (
-    <ButtonStyled $appearance={appearance} className={className} {...rest}>
+    <ButtonStyled
+      $appearance={appearance}
+      $size={size}
+      className={className}
+      {...rest}
+    >
       {text ?? children}
     </ButtonStyled>
   );
