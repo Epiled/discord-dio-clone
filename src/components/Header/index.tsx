c9dc915a2cfb804854a0ee2d27692cf06c0ddcd8
Felipe De Andrade <felipe.deam98@gmail.com>
2026-04-19 12:14:56 -0300

    refactor(header): restructure hero section to match official reference

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,52 @@
 import { Button } from "../Button";
+import { DownloadIcon } from "../Icons/DownloadIcon";
+
 import {
+  HeaderBackground1,
+  HeaderBackground2,
+  HeaderBackground3,
   HeaderButtons,
   HeaderContent,
+  HeaderImages,
   HeaderStyled,
   HeaderText,
   HeaderTitle,
+  HeaderWrapper,
 } from "./styles";
+
+import Background1 from "assets/svg/icons-art/background-1.svg";
+import Background2 from "assets/svg/icons-art/background-2.svg";
+import Background3 from "assets/svg/icons-art/background-3.svg";
 
 export const Header = () => {
   return (
     <HeaderStyled>
-      <HeaderContent>
-        <HeaderTitle>{`Imagine um \n lugar...`}</HeaderTitle>
-        <HeaderText>
-          …onde você possa pertencer a um clube escolar, um grupo de gamers, ou
-          uma comunidade artística mundial. Onde você e alguns amigos possam
-          passar um tempo juntos. Um lugar que torna fácil conversar todos os
-          dias e socializar com mais frequência.
-        </HeaderText>
-        <HeaderButtons>
-          <Button text="Baixar para Windows" />
-          <Button appearance="inverse" text="Abra o Discord no seu navegador" />
-        </HeaderButtons>
-      </HeaderContent>
+      <HeaderWrapper>
+        <HeaderContent>
+          <HeaderTitle>{`Imagine um lugar...`}</HeaderTitle>
+          <HeaderText>
+            …onde você possa pertencer a um clube escolar, um grupo de gamers,
+            ou uma comunidade artística mundial. Onde você e alguns amigos
+            possam passar um tempo juntos. Um lugar que torna fácil conversar
+            todos os dias e socializar com mais frequência.
+          </HeaderText>
+          <HeaderButtons>
+            <Button
+              icon={<DownloadIcon aria-hidden="true" />}
+              text="Baixar para Windows"
+            />
+            <Button
+              appearance="inverse"
+              text="Abra o Discord no seu navegador"
+            />
+          </HeaderButtons>
+        </HeaderContent>
+      </HeaderWrapper>
+      <HeaderImages>
+        <HeaderBackground3 src={Background3} />
+        <HeaderBackground2 src={Background2} />
+        <HeaderBackground1 src={Background1} />
+      </HeaderImages>
     </HeaderStyled>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,68 +1,171 @@
 import styled from "styled-components";
 
-import HeaderImage from "assets/img/header.png"
-
 export const HeaderStyled = styled.header`
-  background: url(${HeaderImage});
-  background-size: cover;
+  background: var(--color-surface-banner);
+  background-size: 100%, cover;
   background-position: center;
-  min-height: 46rem;
-  padding: 7rem 0;
-  align-content: center;
-  justify-content: center;
-  justify-items: center;
+  min-height: 62.6rem;
+  padding: 8rem 2.4rem 0 2.4rem;
+  position: relative;
 
   @media screen and (min-width: 768px) {
-    padding: 9.4rem 0;
+    padding: 9.4rem 4rem 8rem 4rem;
+  }
+
+  @media screen and (min-width: 1024px) {
+    padding: 20rem 4rem 12rem 4rem;
+  }
+`
+
+export const HeaderWrapper = styled.div`
+  display: grid;
+  gap: 2rem;
+  position: relative;
+  z-index: 2;
+
+  @media screen and (min-width: 768px) {
+    grid-template-columns: repeat(8, 1fr);
+  }
+
+  @media screen and (min-width: 1024px) {
+    grid-template-columns: repeat(12, 1fr);
   }
 `
 
 export const HeaderContent = styled.div`
-  max-width: 76rem;
-  display: grid;
+  display: flex;
+  flex-direction: column;
   gap: 2.4rem;
-  padding: 0 2.4rem;
+  grid-column: span 5; 
+  padding: 5.6rem 0;
+  max-width: 76rem;
 
   @media screen and (min-width: 768px) {
+    gap: 4rem;
+    padding: 8rem 0;
+  }
+
+  @media screen and (min-width: 1024px) {
+    grid-column: 3 / span 8;
+    text-align: center;
+    align-items: center;
     padding: 0;
+    max-width: 128rem;
   }
 `
 
 export const HeaderTitle = styled.h1`
-  color: rgba(255, 255, 255, 1);
-  font-size: 3.2rem;
+  color: var(--color-neutral-white);
+  font-size: 4.4rem;
+  font-size: clamp(2.4rem, 5vw, 4.4rem);
+  font-weight: 800;
+  font-family: "Ginto Nord", "Whitney", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
   line-height: 100%;
   text-align: left;
   text-transform: uppercase;
+  text-rendering: geometricPrecision;
   white-space: pre-line;
   
   @media screen and (min-width: 768px) {
+    font-size: 5.6rem;
+  }
+
+  @media screen and (min-width: 1024px) {
     text-align: center;
-    font-size: 5.2rem;
   }
 `
 
 export const HeaderText = styled.p`
-  color: rgba(255, 255, 255, 1);
-  font-size: 1.6rem;
+  color: var(--color-neutral-white);
+  font-size: clamp(1.6rem, 2vw, 2rem);
+  line-height: 1.625;
+  font-family: "Whitney", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
   text-align: left;
-  font-family: 'Open Sans', sans-serif;
 
-  @media screen and (min-width: 768px) {
+  @media screen and (min-width: 1024px) {
     text-align: center;
   }
 `
 
 export const HeaderButtons = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 1.6rem;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  gap: 2.4rem;
   
   @media screen and (min-width: 768px) {
     flex-direction: row;
     align-items: center;
-    gap: 2.4rem;
+  }
+
+  @media screen and (min-width: 1024px) {
+    justify-content: center;
+  }
+`
+
+export const HeaderImages = styled.div`
+  @media screen and (min-width: 768px) {
+    position: absolute;
+    inset: 0;
+  }
+`
+
+export const HeaderBackground1 = styled.img`
+  display: none;
+  position: absolute;
+
+  @media screen and (min-width: 768px) {
+    display: block;
+    top: auto;
+    bottom: 0;
+    left: 50%;
+    margin-left: -1320px;
+  }
+
+  @media screen and (min-width: 1024px) {
+    margin-left: -1280px;
+  }
+`
+export const HeaderBackground2 = styled.img`
+  display: none;
+  position: absolute;
+
+  @media screen and (min-width: 768px) {
+    display: block;
+    bottom: 0;
+    left: 50%;
+    margin-left: -70px;
+    z-index: 1;
+  }
+
+  @media screen and (min-width: 1024px) {
+    display: block;
+    bottom: 0;
+    left: 50%;
+    margin-left: 370px;
+  }
+`
+export const HeaderBackground3 = styled.img`
+  position: relative;
+  bottom: 0;
+  left: -8rem;
+  width: 100%;
+
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+
+  @media screen and (min-width: 1024px) {
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    display: block;
+    height: auto;
+    width: auto;
+    max-width: none;
+    margin-left: -1030px;
+    transform: none;
+    z-index: 1;
   }
 `
 
