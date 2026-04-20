b0cbbefc57941bd1eba491e361d3a7dcefbb3772
Felipe De Andrade <felipe.deam98@gmail.com>
2026-04-20 12:49:45 -0300

    refactor(Footer): update padding and max width

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -8,14 +8,28 @@ export const FooterStyled = styled.footer`
   @media screen and (min-width: 764px) {
     padding: 8rem 4rem 6.4rem 4rem;
   }
+
+  @media screen and (min-width: 1024px) {
+    justify-items: center;
+  }
 `
 
 export const FooterMain = styled.div`
   display: grid;
   gap: 5.6rem;
-  border-bottom: .1rem solid var(--color-brand-primary);
   padding-bottom: 4rem;
   margin-bottom: 3.2rem;
+  position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    height: .1rem;
+    background: var(--color-brand-primary);
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
 
   @media screen and (min-width: 764px) {
     grid-template-columns: repeat(8, 1fr);
@@ -24,6 +38,13 @@ export const FooterMain = styled.div`
 
   @media screen and (min-width: 1024px) {
     grid-template-columns: repeat(12, 1fr);
+    max-width: 126rem;
+    padding: 0 4rem 4rem 4rem;
+
+    &::after {
+      left: 4rem;
+      right: 4rem;
+    }
   }
 `
 
@@ -97,6 +118,8 @@ export const FooterList = styled.ul`
 `
 
 export const FootLink = styled(FooterSubtitle).attrs({ as: "a" })`
+  display: flex;
+  align-items: center;
   color: var(--color-neutral-white);
 
   &:hover {
@@ -108,4 +131,10 @@ export const FootLink = styled(FooterSubtitle).attrs({ as: "a" })`
 export const FooterBottom = styled.div`
   display: flex;
   justify-content: space-between;
+  width: 100%;
+  
+  @media screen and (min-width: 1024px) {
+    max-width: 126rem;
+    padding: 0 4rem;
+  }
 `
