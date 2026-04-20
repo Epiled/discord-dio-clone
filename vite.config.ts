a518f16e64c136ad8658fba9b18df47199e961d4
Felipe De Andrade <felipe.deam98@gmail.com>
2026-04-20 15:55:38 -0300

    perf(build): inject preloads for hero assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import UnpluginInjectPreload from 'unplugin-inject-preload/vite'
 import path from "path";
 import { fileURLToPath } from "url";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), UnpluginInjectPreload({
+    files: [
+        {
+          // 'outputMatch' é o nome correto para arquivos no diretório de saída (dist)
+          // Usamos .* no início para ignorar o caminho 'assets/' e (-.*)? para o hash opcional
+          outputMatch: /background-[1-3](-.*)?\.svg$/,
+          attributes: {
+            fetchpriority: 'high',
+            as: 'image',
+            type: 'image/svg+xml',
+          },
+        },
+      ],
+  })],
   resolve: {
     alias: {
       assets: path.resolve(__dirname, "src/assets"),
