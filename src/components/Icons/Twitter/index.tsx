72eea9b20fc46343ac4962ed1d553b82d6284b13
Felipe De Andrade <felipe.deam98@gmail.com>
2026-04-20 08:20:58 -0300

    feat(icons): add Facebook, Instagram, Twitter and Youtube icons as React components

---
+++ src/components/Icons/Twitter/index.tsx
@@ -0,0 +1,4 @@
+import { createIcon } from "..";
+import Twitter from "assets/svg/icons-ui/twitter.svg?react";
+
+export const TwitterIcon = createIcon(Twitter);
---
+++ src/components/Icons/Youtube/index.tsx
@@ -0,0 +1,4 @@
+import { createIcon } from "..";
+import Youtube from "assets/svg/icons-ui/youtube.svg?react";
+
+export const YoutubeIcon = createIcon(Youtube);
