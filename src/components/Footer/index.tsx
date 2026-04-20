027c5350787bae97422cb7f6f73b5fc87118ce5a
Felipe De Andrade <felipe.deam98@gmail.com>
2026-04-20 08:23:23 -0300

    refactor(footer): restructure footer section to match official reference

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,114 @@
-import { FooterStyled } from "./styles";
-
-import DiscordIcon from "assets/svg/discord.svg";
+import { getDeviceOS } from "../../hooks/getDeviceOS";
+import { Button } from "../Button";
+import { FacebookIcon } from "../Icons/Facebook";
+import { InstagramIcon } from "../Icons/Instagram/Index";
+import { TwitterIcon } from "../Icons/Twitter";
+import { YoutubeIcon } from "../Icons/Youtube";
+import { Logo } from "../Logo";
+import { SelectLanguage } from "../SelectLanguage";
+import {
+  FooterBottom,
+  FooterColumn,
+  FooterHeader,
+  FooterList,
+  FooterMain,
+  FooterNav,
+  FooterSocial,
+  FooterStyled,
+  FooterSubtitle,
+  FooterTitle,
+  FootLink,
+} from "./styles";
 
 export const Footer = () => {
+  const os = getDeviceOS();
+
   return (
     <FooterStyled>
-      <img src={DiscordIcon} alt="DiscordIcon" />
+      <FooterMain>
+        <FooterHeader>
+          <FooterTitle text="Imagine um lugar" />
+
+          <SelectLanguage />
+
+          <FooterSocial>
+            <FootLink>
+              <TwitterIcon aria-hidden="true" />
+            </FootLink>
+            <FootLink>
+              <InstagramIcon aria-hidden="true" />
+            </FootLink>
+            <FootLink>
+              <FacebookIcon aria-hidden="true" />
+            </FootLink>
+            <FootLink>
+              <YoutubeIcon aria-hidden="true" />
+            </FootLink>
+          </FooterSocial>
+        </FooterHeader>
+
+        <FooterNav>
+          <FooterColumn>
+            <FooterSubtitle>Produto</FooterSubtitle>
+            <FooterList>
+              <FootLink>Baixar</FootLink>
+              <FootLink>Nitro</FootLink>
+              <FootLink>Status</FootLink>
+            </FooterList>
+          </FooterColumn>
+
+          <FooterColumn>
+            <FooterSubtitle>Empresa</FooterSubtitle>
+            <FooterList>
+              <FootLink>Sobre</FootLink>
+              <FootLink>Empregos</FootLink>
+              <FootLink>Marca</FootLink>
+              <FootLink>Sala de imprensa</FootLink>
+            </FooterList>
+          </FooterColumn>
+
+          <FooterColumn>
+            <FooterSubtitle>Recursos</FooterSubtitle>
+            <FooterList>
+              <FootLink>Faculdade</FootLink>
+              <FootLink>Suporte</FootLink>
+              <FootLink>Segurança</FootLink>
+              <FootLink>Blog</FootLink>
+              <FootLink>Comentários</FootLink>
+              <FootLink>Desenvolvedores</FootLink>
+              <FootLink>StreamKit</FootLink>
+            </FooterList>
+          </FooterColumn>
+
+          <FooterColumn>
+            <FooterSubtitle>Política</FooterSubtitle>
+            <FooterList>
+              <FootLink>Termos</FootLink>
+              <FootLink>Privacidade</FootLink>
+              <FootLink>Configurações de cookies</FootLink>
+              <FootLink>Diretrizes</FootLink>
+              <FootLink>Reconhecimentos</FootLink>
+              <FootLink>Licenças</FootLink>
+              <FootLink>Moderação</FootLink>
+            </FooterList>
+          </FooterColumn>
+        </FooterNav>
+      </FooterMain>
+
+      <FooterBottom>
+        <FootLink>
+          <Logo />
+        </FootLink>
+        {os === "Android" && (
+          <Button appearance="brand" size="small" text="Baixar" />
+        )}
+        {os === "iOS" && (
+          <Button appearance="brand" size="small" text="Baixar" />
+        )}
+        {os === "Desktop/Other" && (
+          <Button appearance="brand" size="small" text="Registrar" />
+        )}
+      </FooterBottom>
     </FooterStyled>
   );
 };
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,9 +1,111 @@
 import styled from "styled-components";
+import { Title } from "../Title";
 
 export const FooterStyled = styled.footer`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 3.6rem;
   background: var(--footer);
+  padding: 8rem 2.4rem 6.4rem 2.4rem;
+
+  @media screen and (min-width: 764px) {
+    padding: 8rem 4rem 6.4rem 4rem;
+  }
+`
+
+export const FooterMain = styled.div`
+  display: grid;
+  gap: 5.6rem;
+  border-bottom: .1rem solid var(--color-brand-primary);
+  padding-bottom: 4rem;
+  margin-bottom: 3.2rem;
+
+  @media screen and (min-width: 764px) {
+    grid-template-columns: repeat(8, 1fr);
+    gap: 0 2rem;
+  }
+
+  @media screen and (min-width: 1024px) {
+    grid-template-columns: repeat(12, 1fr);
+  }
+`
+
+export const FooterHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+
+  @media screen and (min-width: 764px) {
+    grid-column: span 3;
+  }
+`
+
+export const FooterTitle = styled(Title)`
+  color: var(--color-brand-primary);
+  font-size: 3.2rem;
+  font-weight: 800;
+  line-height: 95%;
+  text-transform: uppercase;
+  text-rendering: geometricPrecision;
+`
+
+export const FooterSubtitle = styled.h3`
+  color: var(--color-brand-primary);
+  font-family: "Whitney", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+  font-size: 1.6rem;
+  font-weight: 300;
+  line-height: 2.4rem;
+`
+
+export const FooterSocial = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 2.4rem;
+  color: var(--color-neutral-white);
+`;
+
+export const FooterNav = styled.nav`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+  grid-template-rows: auto;
+  gap: 4rem 2rem;
+
+  @media screen and (min-width: 764px) {
+    grid-column-start: 5;
+    grid-column-end: 9;
+  }
+
+  @media screen and (min-width: 1024px) {
+    grid-template-columns: repeat(8, minmax(0, 1fr));
+    grid-column-end: 13;
+  }
+`
+
+export const FooterColumn = styled.div`
+  grid-column: span 1;
+  display: flex;
+  flex-direction: column;
+  gap: .8rem;
+
+  @media screen and (min-width: 1024px) {
+    grid-column: span 2;
+  }
+`
+
+export const FooterList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: .8rem;
+`
+
+export const FootLink = styled(FooterSubtitle).attrs({ as: "a" })`
+  color: var(--color-neutral-white);
+
+  &:hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+`
+
+export const FooterBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
 `
