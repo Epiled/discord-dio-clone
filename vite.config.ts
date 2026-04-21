ac812da787fd3f2ec61df86d84258db0fe871ca7
Felipe De Andrade <felipe.deam98@gmail.com>
2026-04-21 10:07:09 -0300

    chore(setup): config imagetools and UnpluginInjectPreload

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { imagetools } from 'vite-imagetools'
 import UnpluginInjectPreload from 'unplugin-inject-preload/vite'
 import path from "path";
 import { fileURLToPath } from "url";
@@ -10,18 +11,24 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), svgr(), UnpluginInjectPreload({
-    files: [
-        {
-          outputMatch: /background-[1-3](-.*)?\.svg$/,
-          attributes: {
-            fetchpriority: 'high',
-            as: 'image',
-            type: 'image/svg+xml',
+  plugins: [
+    react(), 
+    svgr(), 
+    imagetools(),
+    UnpluginInjectPreload({
+      files: [
+          {
+            outputMatch: /background-[1-3](-.*)?\.svg$/,
+            attributes: {
+              fetchpriority: 'high',
+              as: 'image',
+              type: 'image/svg+xml',
+            },
           },
-        },
-      ],
-  })],
+        ],
+    }),
+
+  ],
   resolve: {
     alias: {
       assets: path.resolve(__dirname, "src/assets"),
